881dbb53025b3feef4543f9801c6f8fe2c34f977
Harsh Patel <harshp@bos-wprqw.kendall.corp.akamai.com>
2019-07-18 15:22:56 -0400

    Refactored code and libraries

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
 jQuery(document).ready(function( $ ) {
 
-  // Smooth scroll for the menu and links with .scrollto classes
   $('.smothscroll').on('click', function(e) {
     e.preventDefault();
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
-
         $('html, body').animate({
           scrollTop: target.offset().top - 62
         }, 1500, 'easeInOutExpo');
       }
     }
   });
-
-
-
-
 
  });
